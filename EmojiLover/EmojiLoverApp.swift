cc25185b5b70a4c59b2e9dd3b9a084d6fa8246f8
Rishi Srinivasan <rishi.cv40@gmail.com>
2024-02-05 20:49:41 +0100

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Rishi Srinivasan on 05.02.24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
